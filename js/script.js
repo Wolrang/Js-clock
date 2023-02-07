3fc727a9b69bc8e04c89fac8cafb9e381378581f
Wolrang <dkfud252@naver.com>
2023-02-07 17:24:21 +0900

    🎉 js-clock

---
+++ js/script.js
@@ -0,0 +1,65 @@
+let clock = document.getElementById("clock")
+
+function getClock() {
+  let now = new Date();
+  let H = now.getHours();
+  let M = now.getMinutes();
+  let S = now.getSeconds();
+  clock.innerText = `${H} : ${M} : ${S}`
+  if(S < "10") {
+    clock.innerText = `${H} : ${M} : ${"0" + S}`
+  } else if (M < "10") {
+    lock.innerText = `${H} : ${"0" + M} : ${S}`
+  } else if (H < "10") {
+    lock.innerText = `${"0" + H} : ${M} : ${S}`
+  } else {
+    clock.innerText = `${H} : ${M} : ${S}`
+  }
+};
+getClock();
+setInterval(getClock, 1000);
+
+$("#morning").click(function() {
+  $("#bgc-img").css({"background":"url(./images/bg_morning.jpg)"});
+  $("#ph-img").css({"background":"url(./images/phone_morning.png)"});
+  $("#morning, .morning").addClass('active')
+  $("#afternoon, #evening, #night").removeClass('active')
+  $(".afternoon, .evening, .night").removeClass('active')
+  $("#logo").attr("src", "./images/logo_b.png")
+  $(".gnb-item, .text1, h1").css("color", "#000")
+  $(".secondary-text, .text2").css("color", "#000")
+
+});
+
+$("#afternoon").click(function() {
+  $("#bgc-img").css({"background":"url(./images/bg_afternoon.jpg)"});
+  $("#ph-img").css({"background":"url(./images/phone_afternoon.png)"});
+  $("#afternoon, .afternoon").addClass('active')
+  $("#morning, #evening, #night").removeClass('active')
+  $(".morning, .evening, .night").removeClass('active')
+  $("#logo").attr("src", "./images/logo_b.png")
+  $(".gnb-item, .text1, h1").css("color", "#000")
+  $(".secondary-text, .text2").css("color", "#000")
+});
+
+$("#evening").click(function() {
+  $("#bgc-img").css({"background":"url(./images/bg_evening.jpg)"});
+  $("#ph-img").css({"background":"url(./images/phone_evening.png)"});
+  $("#evening, .evening").addClass('active')
+  $("#morning, #afternoon, #night").removeClass('active')
+  $(".morning, .afternoon, .night").removeClass('active')
+  $("#logo").attr("src", "./images/logo_b.png")
+  $(".gnb-item, .text1, h1").css("color", "#000")
+  $(".secondary-text, .text2").css("color", "#000")
+});
+
+$("#night").click(function() {
+  $("#bgc-img").css({"background":"url(./images/bg_night.jpg)"});
+  $("#ph-img").css({"background":"url(./images/phone_night.png)"});
+  $("#night, .night").addClass('active')
+  $("#morning, #afternoon, #evening").removeClass('active')
+  $(".morning, .afternoon, .evening").removeClass('active')
+  $("#logo").attr("src", "./images/logo_w.png")
+  $(".gnb-item, .text1, h1").css("color", "#fff")
+  $(".secondary-text, .text2").css("color", "#666")
+});
